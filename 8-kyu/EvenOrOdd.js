95fe1268af2d28be1d1e849684978c7a75801cca
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2025-10-23 23:51:10 +0300

    feat: add Even or Odd solution

---
+++ 8-kyu/EvenOrOdd.js
@@ -0,0 +1,14 @@
+/*
+* Create a function that takes an integer as an argument and returns "Even" 
+* for even numbers or "Odd" for odd numbers.ё
+*/
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+// Test cases
+console.log(evenOrOdd(2));    // "Even"
+console.log(evenOrOdd(7));    // "Odd" 
+console.log(evenOrOdd(-4));   // "Even"
+console.log(evenOrOdd(0));    // "Even"
+console.log(evenOrOdd(1));    // "Odd"
