0f5e372e486ae87400072fb506a23429a581ccc7
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2025-10-23 23:24:48 +0300

    feat: add Find Letter Number solution

---
+++ 8-kyu/FindLetterNumber.js
@@ -0,0 +1,13 @@
+/*
+ * When provided with a letter, return its position in the alphabet.
+ * Input: "a"
+ * Output: "Position of alphabet: 1"
+ * Note: Only lowercased English letters are tested
+ */
+function position(letter){
+  return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
+}
+
+// Test cases
+console.log(position("a")); // "Position of alphabet: 1"
+console.log(position("z")); // "Position of alphabet: 26"
