bedd2b430836f6b9f7c759c323f6710204910f3e
Sentyabrina Sulakova <sentyabrinasulakova@gmail.com>
2025-10-24 13:05:56 +0300

    style: improve code formatting and comments

--- 8-kyu/EvenOrOdd.js
+++ 8-kyu/EvenOrOdd.js
@@ -1,6 +1,6 @@
 /*
- * Create a function that takes an integer as an argument and returns "Even"
- * for even numbers or "Odd" for odd numbers.ё
+  Create a function that takes an integer as an argument and returns "Even"
+  for even numbers or "Odd" for odd numbers.ё
  */
 function evenOrOdd(number) {
   return number % 2 === 0 ? "Even" : "Odd";
--- 8-kyu/FindLetterNumber.js
+++ 8-kyu/FindLetterNumber.js
@@ -1,10 +1,10 @@
 /*
- * When provided with a letter, return its position in the alphabet.
- * Input: "a"
- * Output: "Position of alphabet: 1"
- * Note: Only lowercased English letters are tested
+  When provided with a letter, return its position in the alphabet.
+  Input: "a"
+  Output: "Position of alphabet: 1"
+  Note: Only lowercased English letters are tested
  */
-function position(letter){
+function position(letter) {
   return `Position of alphabet: ${letter.charCodeAt(0) - 96}`;
 }
 
